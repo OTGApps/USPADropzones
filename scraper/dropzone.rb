e995a5e4417a4ec1e5cdf87a36ea0e81c1f78e80
Mark Rickert <mjar81@gmail.com>
2016-03-24 23:21:51 -0400

    Whoops. forgot to remove the string methods from the scraper.

--- scraper/dropzone.rb
+++ scraper/dropzone.rb
@@ -4,27 +4,6 @@ require 'bundler'
 Bundler.require
 require './string'
 
-class String
-  def is_i?
-    /\A[-+]?\d+\z/ === self
-  end
-
-  def string_between_markers marker1, marker2
-    self[/#{Regexp.escape(marker1)}(.*?)#{Regexp.escape(marker2)}/m, 1]
-  end
-
-  def titleize
-    split(/(\W)/).map(&:capitalize).join
-  end
-
-  def underscore
-    self.gsub(/::/, '/').
-    gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-    gsub(/([a-z\d])([A-Z])/,'\1_\2').
-    tr("-", "_").
-    downcase
-  end
-end
 
 class DZScraper
   def initialize
