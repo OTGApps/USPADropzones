054ac4a2efa96d83723bc6433288a46e8bc742ba
Mark Rickert <mjar81@gmail.com>
2018-10-25 14:17:55 -0700

    Update scraper and local files for new side design

--- scraper/dropzone.rb
+++ scraper/dropzone.rb
@@ -23,10 +23,11 @@ class DZScraper
     # Open the file
     page = parse_file(file_name)
     # Get the links to the different dropzones in the state:
-    dz_ids = page.css('a').select{|alink| alink.text.chomp == "View Details"}.map{|l| l[:href].split("=").last }
+    dz_ids = page.css('a').select{|alink| (alink['href'] || '').include?("DZdetails")}.map{|l| l[:href].split("=").last }
     dz_ids.each do |loc_id|
       dz_file_name = "local_files/#{folder}/#{abbrev}/#{loc_id}.html"
       unless File.file?(dz_file_name)
+        puts "Writing file to: " + dz_file_name
         url = location_url(loc_id)
         agent.get(url).save(dz_file_name)
       end
@@ -55,6 +56,7 @@ class DZScraper
     countries.each do |abbrev|
       file_name = "local_files/international/#{abbrev}.html"
       unless File.file?(file_name)
+        pp "Writing File: " + file_name
         url = country_page_url(abbrev)
         agent.get(url).save(file_name)
       end
@@ -305,19 +307,19 @@ class DZScraper
   def countries
     # Dir.chdir('local_files/international')
     # Dir.glob('*').select {|f| File.directory? f}.sort
-    ["AR", "BE", "BR", "BG", "CA", "CL", "CN", "CR", "HR", "CZ", "DK", "FI", "FR", "DE", "GR", "HU", "IN", "IE", "IL", "IT", "JP", "KE", "MX", "MA", "NA", "NL", "PH", "PL", "PT", "QA", "RO", "RU", "ES", "CH", "TH", "UA", "AE", "VI", "VE"]
+    %w(AF AX AL DZ AS AD AO AI AQ AG AR AM AW AU AT AZ BS BH BD BB BY BE BZ BJ BM BT BO BQ BA BW BV BR IO BN BG BF BI KH CM CA CV KY CF TD CL CN CX CC CO KM CG CD CK CR CI HR CU CW CY CZ DK DJ DM DO EC EG SV GQ ER EE ET FK FO FJ FI FR GF PF TF GA GM GE DE GH GI GR GL GD GP GU GT GG GN GW GY HT HM VA HN HK HU IS IN ID IR IQ IE IM IL IT JM JP JE JO KZ KE KI KP KR KW KG LA LV LB LS LR LY LI LT LU MO MK MG MW MY MV ML MT MH MQ MR MU YT MX FM MD MC MN ME MS MA MZ MM NA NR NP NL NC NZ NI NE NG NU NF MP NO OM PK PW PS PA PG PY PE PH PN PL PT PR QA RE RO RU RW BL SH KN LC MF PM VC WS SM ST SA SN RS SC SL SG SX SK SI SB SO ZA GS SS ES LK SD SR SJ SZ SE CH SY TW TJ TZ TH TL TG TK TO TT TN TR TM TC TV UG UA AE GB UM UY UZ VU VE VN VG VI WF EH YE ZM ZW)
   end
 
   def state_page_url(state)
-    "http://www.uspa.org/Drop-Zone-Locator/region/#{state}"
+    "https://uspa.org/dzlocator?DZ=&City=&State=#{state}&Region=&CountryHidden=&Country=&FindaDZ=Find"
   end
 
   def country_page_url(country)
-    "http://www.uspa.org/Drop-Zone-Locator/country/#{country}"
+    "https://uspa.org/dzlocator?DZ=&City=&State=&Region=&CountryHidden=&Country=#{country}&FindaDZ=Find"
   end
 
   def location_url(location)
-    "http://www.uspa.org/Drop-Zone-Locator?location=#{location}"
+    "https://uspa.org/DZdetails?accountnumber=#{location}"
   end
 end
 
